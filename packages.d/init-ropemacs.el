e6bd5e1b79bacc86d89fadc37cd367be1677e9cb
Sebastien Beal <sebastibe@gmail.com>
2014-03-24 09:46:10 +0900

    Small pymacs fixups

--- packages.d/init-ropemacs.el
+++ packages.d/init-ropemacs.el
@@ -10,18 +10,18 @@
 
   (setq ropemacs-guess-project t)
   (setq ropemacs-enable-autoimport t)
-  
+
   (setq ropemacs-autoimport-modules '("os" "shutil" "sys" "logging"))
 
   ;; Adding hook to automatically open a rope project if there is one
   ;; in the current or in the upper level directory
   (add-hook 'python-mode-hook
-	    (lambda ()
-	      (cond ((file-exists-p ".ropeproject")
-		     (rope-open-project default-directory))
-		    ((file-exists-p "../.ropeproject")
-		     (rope-open-project (concat default-directory "..")))
-		    )))
+            (lambda ()
+              (cond ((file-exists-p ".ropeproject")
+                     (rope-open-project default-directory))
+                    ((file-exists-p "../.ropeproject")
+                     (rope-open-project (concat default-directory "..")))
+                    )))
   )
 
 ;; Python or python mode?
@@ -39,8 +39,8 @@
        "Activate a Virtual Environment specified by PATH" t)
      (autoload 'virtualenv-workon "virtualenv"
        "Activate a Virtual Environment present using virtualenvwrapper" t)
-     
-     
+
+
      ;; Not on all modes, please
      (add-hook 'python-mode-hook 'flymake-find-file-hook)
 
@@ -52,6 +52,6 @@
        (virtualenv-activate virtualenv)
        (desktop-change-dir virtualenv))
 
-     
+
      )
   )
