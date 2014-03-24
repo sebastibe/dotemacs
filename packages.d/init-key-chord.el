8d13774f361cc29f91571edc5d4aaf6a8946988d
Sebastien Beal <sebastibe@gmail.com>
2014-03-24 09:44:40 +0900

    Move browse-kill-ring to key-chord yy

--- packages.d/init-key-chord.el
+++ packages.d/init-key-chord.el
@@ -3,5 +3,7 @@
 (key-chord-define-global "jk" 'ace-jump-char-mode)
 (key-chord-define-global "JJ" 'preamble-switch-to-previous-buffer)
 (key-chord-define-global "uu" 'undo-tree-visualize)
+(key-chord-define-global "ww" 'kill-ring-save)
+(key-chord-define-global "yy" 'browse-kill-ring)
 
 (key-chord-mode +1)
